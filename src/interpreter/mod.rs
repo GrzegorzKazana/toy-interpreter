a4e28bac67df5ac66f2b2ce0b2e7940b5a9b617b
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2020-04-14 18:12:16 +0200

    refactor: move getting input to main

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,10 +1,8 @@
-use crate::io::CommandLine;
 use crate::parser::Parser;
 use crate::runtime::Runtime;
 use crate::tokenizer::InputTokenizer;
 
 pub struct Interpreter {
-    command_line: CommandLine,
     tokenizer: InputTokenizer,
     parser: Parser,
     runtime: Runtime,
@@ -13,7 +11,6 @@ pub struct Interpreter {
 impl Interpreter {
     pub fn new() -> Self {
         Interpreter {
-            command_line: CommandLine {},
             tokenizer: InputTokenizer {},
             parser: Parser {},
             runtime: Runtime::new(),
@@ -22,20 +19,7 @@ impl Interpreter {
 }
 
 impl Interpreter {
-    pub fn run(&mut self) {
-        while let Option::Some(input_str) = self.command_line.get_input() {
-            match input_str.trim() {
-                "exit" => break,
-                "" => continue,
-                _ => match self.handle_input(&input_str) {
-                    Result::Ok(msg) => self.command_line.print_output(msg),
-                    Result::Err(err_msg) => self.command_line.print_output(err_msg),
-                },
-            }
-        }
-    }
-
-    fn handle_input(&mut self, input_str: &str) -> Result<String, String> {
+    pub fn handle_input(&mut self, input_str: &str) -> Result<String, String> {
         let tokens = self.tokenizer.tokenize(&input_str)?;
         let ast = self.parser.parse(&tokens)?;
         let result = self.runtime.exec(&ast)?;
--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,17 @@ mod visitor;
 // fun fac(n) = n ? n * fac(n - 1) : 1
 
 fn main() {
-    interpreter::Interpreter::new().run();
+    let command_line = io::CommandLine {};
+    let mut interpreter = interpreter::Interpreter::new();
+
+    while let Option::Some(input_str) = command_line.get_input() {
+        match input_str.trim() {
+            "exit" => break,
+            "" => continue,
+            _ => match interpreter.handle_input(&input_str) {
+                Result::Ok(msg) => command_line.print_output(msg),
+                Result::Err(err_msg) => command_line.print_output(err_msg),
+            },
+        }
+    }
 }
